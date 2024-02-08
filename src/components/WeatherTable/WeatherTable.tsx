497d1bfafa501c902511c41b59e330837a87fd00
Olya <okatranis@gmail.com>
2024-02-08 15:24:08 +0200

    Weather Table for month

---
+++ src/components/WeatherTable/WeatherTable.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import WeatherTableMonthDate from "../WeatherTableMonthDate/WeatherTableMonthDate";
+import styles from './WeatherTable.module.scss';
+
+interface Data  {
+    id: number;
+    dayOfWeek: string;
+    date: number;
+    icon: string;
+    maxDegree: string;
+    minDegree: string;
+    accent?: boolean;
+}
+
+const WeatherTable = () => {
+    const weatherData:Data[] = [
+        {
+            id: 1,
+            dayOfWeek: 'Пн',
+            date: 3,
+            icon: 'string',
+            maxDegree: '+4',
+            minDegree: '-5',
+        },
+        {
+            id: 2,
+            dayOfWeek: 'вт',
+            date: 4,
+            icon: 'string',
+            maxDegree: '+4',
+            minDegree: '-5',
+        },
+        {
+            id: 3,
+            dayOfWeek: 'ср',
+            date: 5,
+            icon: 'string',
+            maxDegree: '+4',
+            minDegree: '-5',
+        },
+        {
+            id: 4,
+            dayOfWeek: 'чт',
+            date: 6,
+            icon: 'string',
+            maxDegree: '+4',
+            minDegree: '-5',
+        },
+        {
+            id: 5,
+            dayOfWeek: 'пт',
+            date: 7,
+            icon: 'string',
+            maxDegree: '+4',
+            minDegree: '-5',
+        },
+        {
+            id: 6,
+            dayOfWeek: 'сб',
+            date: 8,
+            icon: 'string',
+            maxDegree: '+4',
+            minDegree: '-5',
+            accent: true,
+        },
+        {
+            id: 7,
+            dayOfWeek: 'нд',
+            date: 9,
+            icon: 'string',
+            maxDegree: '+4',
+            minDegree: '-5',
+            accent: true,
+        },
+    ];
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.month_wrap}>
+                <div className={styles.month_week}>
+                    {weatherData.map(item => (
+                        <WeatherTableMonthDate
+                            key={item.id}
+                            dayOfWeek={item.dayOfWeek}
+                            date={item.date}
+                            accent={item.accent}
+                        />
+                    ))}
+                </div>
+                <div className={styles.month_week}>
+                    {weatherData.map(item => (
+                        <WeatherTableMonthDate
+                            key={item.id}
+                            dayOfWeek={item.dayOfWeek}
+                            date={item.date}
+                            accent={item.accent}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default WeatherTable;
