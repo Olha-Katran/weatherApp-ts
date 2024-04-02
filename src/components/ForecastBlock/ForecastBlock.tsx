43129421f9fa153dbd84bc7f3fe1ce952475ef97
Olya <okatranis@gmail.com>
2024-04-02 12:22:06 +0300

    add scroll

--- src/components/ForecastBlock/ForecastBlock.tsx
+++ src/components/ForecastBlock/ForecastBlock.tsx
@@ -6,14 +6,10 @@ const tempor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const ForecastBlock:React.FC = () => {
     return (
-        <div className={styles.forecast_box}>
-            <div className={styles.forecast_block}>
-                <div className={styles.forecast_wrapper}>
-                    {tempor.map(element => (
-                        <ForecastPhase key={element} index={element} />
-                    ) )}
-                </div>
-            </div>
+        <div className={styles.forecast_wrapper}>
+            {tempor.map(element => (
+                <ForecastPhase key={element} index={element} />
+            ) )}
         </div>
     );
 };
