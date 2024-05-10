35d58718b3cb7844183bf0d9cc4c85ed7d33fc10
Olya <okatranis@gmail.com>
2024-05-10 12:11:32 +0300

    MainForecast Component

--- src/components/ForecastComponent/ForecastComponent.tsx
+++ src/components/ForecastComponent/ForecastComponent.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import styles from './ForecastComponent.module.scss';
 import ReturnButton from "../../shared/ReturnButton/ReturnButton";
+import TomorrowCard from "../TomorrowCard/TomorrowCard";
+import styles from './ForecastComponent.module.scss';
+import {useAppSelector} from "../../redux/hooks";
+import {useGetHourlyForecastQuery} from "../../redux/api/hourly-forecast";
+import ShortForecastList from "../ShortForecastList/ShortForecastList";
 
 const ForecastComponent = () => {
+    const selectedCity = useAppSelector((state) => state.cityData);
+
+    const {
+        data: forecast,
+        isLoading,
+        error
+    } = useGetHourlyForecastQuery(selectedCity.coord);
+
     return (
-        <>
-            <ReturnButton name='Next 7 days' />
-            Component
-        </>
+        <div className={styles.forecast_container}>
+            {error ? (
+                <>Error</>
+            ) : isLoading ? (
+                <>Loading...</>
+            ): forecast ? (
+                <>
+                    <ReturnButton name='Next 5 days' />
+                    <TomorrowCard data={forecast}/>
+                    <ShortForecastList data={forecast} />
+                </>
+            ): null}
+        </div>
     )
 };
 
