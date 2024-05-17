e0051ce3f3b9dc0cada21fa7f3097b77d4c29c14
Olya <okatranis@gmail.com>
2024-05-17 12:18:37 +0300

    add expanded forecast card

--- src/components/ShortForecastList/ShortForecastList.tsx
+++ src/components/ShortForecastList/ShortForecastList.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {HourlyForecastResponse} from "../../types/HourlyForecast";
 import ShortWeatherInfoHeader from "../ShortWeatherInfoHeader/ShortWeatherInfoHeader";
 import styles from './ShortForecastList.module.scss';
+import TomorrowList from "../TomorrowList/TomorrowList";
 
 type Props = {
     data: HourlyForecastResponse;
@@ -9,22 +10,40 @@ type Props = {
 
 
 const ShortForecastList: React.FC<Props> = ({ data }) => {
+    const [selectedItem, setSelectedItem] = useState<number | null>(0);
     const dailyAverages = data.list.filter(item => {
         const itemDate = new Date(item.dt * 1000);
         return itemDate.getHours() === 15;
     });
 
+    const toggleList = (index: number): void => {
+        setSelectedItem(index === selectedItem ? null : index);
+        console.log(selectedItem);
+    }
+
     return (
-        <div className={styles.short_forecast_list_container}>
-            {dailyAverages.map((item) => (
-                <ShortWeatherInfoHeader
-                    key={item.dt}
-                    dayOfWeek={item.dt}
-                    temp={item.main.temp}
-                    iconUrl={item.weather[0].icon}
-                />
+        <ul className={styles.short_forecast_list_container}>
+            {dailyAverages.map((item, index) => (
+                <li key={item.dt} className={`${styles.short_forecast_list_item} ${index == selectedItem ? styles.active : '' }`}>
+                    <ShortWeatherInfoHeader
+                        index={index}
+                        dayOfWeek={item.dt}
+                        temp={item.main.temp}
+                        iconUrl={item.weather[0].icon}
+                        onClick={() => toggleList(index)}
+                    />
+                    {index === selectedItem && (
+                        <div className={styles.hidden_element}>
+                            <TomorrowList
+                                wind={item.wind.speed}
+                                rain={item.rain ? item.rain['3h'] : 0}
+                                humidity={item.main.humidity}
+                            />
+                        </div>
+                    )}
+                </li>
             ))}
-        </div>
+        </ul>
     )
 };
 
--- src/components/ShortWeatherInfoHeader/ShortWeatherInfoHeader.tsx
+++ src/components/ShortWeatherInfoHeader/ShortWeatherInfoHeader.tsx
@@ -3,20 +3,24 @@ import styles from './ShortWeatherInfoHeader.module.scss';
 import {formatTemperature} from "../../shared/FormatTemperature/FormatTemperature";
 
 type Props = {
+    index: number;
     dayOfWeek: number | string;
     temp: number;
     iconUrl: string;
     color?: boolean;
+    onClick?: (index: number) => void;
 };
 
 const dayNames: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 
 const ShortWeatherInfoHeader:React.FC<Props> = ({
+                                                    index,
                                                     dayOfWeek,
                                                     temp,
                                                     iconUrl,
                                                     color = false,
+                                                    onClick
 }) => {
     const temperature = formatTemperature(temp);
     let displayDayOfWeek = '';
@@ -30,7 +34,7 @@ const ShortWeatherInfoHeader:React.FC<Props> = ({
     }
 
     return(
-        <div className={`${styles.info_container} ${color ? styles.color : ''}`}>
+        <div className={`${styles.info_container} ${color ? styles.color : ''}`} onClick={() => onClick && onClick(index)}>
             <p>{displayDayOfWeek}</p>
             <div className={styles.icon_temperature_info}>
                 <p className={styles.degree}>{temperature}</p>
