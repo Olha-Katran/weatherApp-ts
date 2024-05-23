03099a23ea534fadb819b2200229081d9c28d1cd
Olya <okatranis@gmail.com>
2024-05-23 15:21:16 +0300

    delete unused components

--- src/router/BrowserRouter.tsx
+++ src/router/BrowserRouter.tsx
@@ -5,8 +5,6 @@ import {
 } from "react-router-dom";
 import PATH from "./routes";
 import Root from "../pages/Root";
-import Today from "../pages/today/Today";
-import Month from "../pages/month/Month";
 import CurrentWeather from "../pages/currentWeather/currentWeather";
 import Forecast from "../pages/Forecast/Forecast";
 
@@ -17,8 +15,6 @@ const BrowserRouter = createBrowserRouter(
                 <Route index element={<CurrentWeather />} />
                 <Route path={PATH.FORECAST} element={<Forecast />} />
             </Route>
-            <Route path={PATH.TODAY} element={<Today />} />
-            <Route path={PATH.MONTH} element={<Month />} />
         </Route>
     )
 );
--- src/shared/Spacer/Spacer.types.ts
+++
@@ -1,3 +0,0 @@
-export enum Units {
-    px = 'px',
-}
